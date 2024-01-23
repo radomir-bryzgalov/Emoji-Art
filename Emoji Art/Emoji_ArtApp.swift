df2771255ac9734f6f1e100256b0ef6c3ea4aa5a
Radomir Bryzgalov <rb8745359@gmail.com>
2024-01-23 08:31:32 -0500

    Initial Commit

---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Радомир Радомир on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
