b873ac889000b36600286d08a1043539f472df7a
Radomir Bryzgalov <rb8745359@gmail.com>
2024-01-25 13:30:25 -0500

    lec 11 38:28

--- Emoji Art/EmojiArtDocument.swift
+++ Emoji Art/EmojiArtDocument.swift
@@ -5,4 +5,47 @@
 //  Created by Радомир Радомир on 1/23/24.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    typealias Emoji = EmojiArt.Emoji
+    
+    @Published private var emojiArt = EmojiArt()
+    
+    var emojis: [Emoji] {
+        emojiArt.emojis
+    }
+    
+    init(){
+        emojiArt.addEmoji("👑", at: .init(x: -200, y: 150), size: 200)
+        emojiArt.addEmoji("👢", at: .init(x: 250, y: -150), size: 80)
+    }
+    
+    var backround: URL? {
+        emojiArt.backround
+    }
+    
+    //MARK: - Intent(s)
+    
+    func setBackroung(_ url: URL?) {
+        emojiArt.backround = url
+    }
+    func addEmoji(_ emoji: String, at position: Emoji.Position, size: CGFloat){
+        emojiArt.addEmoji(emoji, at: position, size: Int(size))
+    }
+    
+}
+
+extension EmojiArt.Emoji{
+    var font: Font {
+        Font.system(size: CGFloat(size))
+    }
+}
+
+extension EmojiArt.Emoji.Position{
+    func `in` (_ geometry: GeometryProxy) -> CGPoint{
+        let center = geometry.frame(in: .local).center
+        return CGPoint(x: center.x + CGFloat(x), y: center.y - CGFloat(y))
+        
+    }
+}
--- Emoji Art/EmojiArtDocumentView.swift
+++ Emoji Art/EmojiArtDocumentView.swift
@@ -8,11 +8,130 @@
 import SwiftUI
 
 struct EmojiArtDocumentView: View {
+    typealias Emoji = EmojiArt.Emoji
+    @ObservedObject var document: EmojiArtDocument
+    
+    private let emojis = "🧳🌂☂️🧵🪡🪢🪭🧶👓🕶🥽🥼🦺👔👕👖🧣🧤🧥🧦👗👘🥻🩴🩱🩲🩳👙👚👛👜👝🎒👞👟🥾🥿👠👡🩰👢👑👒🎩🎓🧢⛑🪖💄💍💼🧳🌂☂️🧵🪡🪢🪭🧶👓🕶🥽🥼🦺👔👕👖🧣🧤🧥🧦👗👘🥻🩴🩱🩲🩳👙👚👛👜👝🎒👞👟🥾🥿👠👡🩰👢👑👒🎩🎓🧢⛑🪖💄💍💼"
+    
+    private let palleteEmojiSize: CGFloat = 40
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing:0){
+            documentBody
+            ScrollingEmojis(emojis)
+                .font(.system(size: palleteEmojiSize))
+                .padding(.horizontal)
+                .scrollIndicators(.hidden)
+        }
+    }
+    private var documentBody: some View {
+        GeometryReader{ geometry in
+            ZStack{
+                Color.white
+                documentContents(in: geometry)
+                    .scaleEffect(zoom * gestureZoom)
+                    .offset(pan + gesturePan)
+                
+            }
+            .gesture(panGesture.simultaneously(with: zoomGesture))
+            .dropDestination(for: Sturldata.self) {sturldata, location in
+                return drop(sturldata, at: location, in: geometry)
+            }
+        }
+    }
+    
+    @State private var zoom: CGFloat = 0.5
+    @State private var pan: CGOffset = .zero
+    
+    @GestureState private var gestureZoom: CGFloat = 1
+    @GestureState private var gesturePan: CGOffset = .zero
+    
+    private var zoomGesture: some Gesture {
+        MagnificationGesture()
+            .updating($gestureZoom) { inMotionPinchScale, gestureZoom, _ in
+                gestureZoom = inMotionPinchScale
+            }
+            .onEnded { endingPinchScale in
+                zoom *= endingPinchScale
+                
+            }
+    }
+    
+    private var panGesture: some Gesture {
+        DragGesture()
+            .updating($gesturePan) { value, gesturePan, _ in
+                gesturePan = value.translation
+            }
+            .onEnded { value in
+                pan += value.translation
+            }
+    }
+    
+    @ViewBuilder
+    private func documentContents(in geometry: GeometryProxy) -> some View {
+        AsyncImage(url: document.backround)
+            .position(Emoji.Position.zero.in(geometry))
+        ForEach(document.emojis) { emoji in
+            Text(emoji.string)
+                .font(emoji.font)
+                .position(emoji.position.in(geometry))
+        }
+
+    }
+    
+    private func drop(_ sturldatas: [Sturldata], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
+        for sturldata in sturldatas {
+            switch sturldata {
+            case .url(let url):
+                document.setBackroung(url)
+                return true
+            case .string(let emoji):
+                document.addEmoji(
+                    emoji,
+                    at: emojiPosition(at: location, in: geometry),
+                    size: palleteEmojiSize / zoom
+                )
+                return true
+            default:
+                break
+            }
+        }
+        return false
+    }
+    
+    
+    private func emojiPosition(at location: CGPoint, in geometry: GeometryProxy) -> Emoji.Position {
+        let center = geometry.frame(in: .local).center
+        return Emoji.Position(
+            x: Int((location.x - center.x - pan.width) / zoom) ,
+            y: Int((-(location.y - center.y - pan.height)) / zoom)
+        )
     }
 }
 
+
+struct ScrollingEmojis: View {
+    let emojis: [String]
+    
+    init(_ emojis: String) {
+        self.emojis = emojis.uniqued.map{ String($0)}
+    }
+    
+    var body: some View{
+        ScrollView(.horizontal){
+            HStack{
+                ForEach(emojis, id: \.self) {emoji in
+                    Text(emoji)
+                        .draggable(emoji)
+                }
+            }
+        }
+    }
+}
+
+
+
+
 #Preview {
-    EmojiArtDocumentView()
+    EmojiArtDocumentView(document: EmojiArtDocument())
 }
